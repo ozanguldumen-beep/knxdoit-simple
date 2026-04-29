c4aed3b20d7862a50e9e01bebc41401c6e640bd1
ozanguldumen-beep <ozanguldumen@gmail.com>
2026-04-29 19:42:18 +0300

    Update app.js

---
+++ static/app.js
@@ -0,0 +1,15 @@
+function addRoom() {
+    alert("Oda eklendi");
+}
+
+function addPanel() {
+    alert("Pano eklendi");
+}
+
+function setTool(tool) {
+    alert("Seçilen: " + tool);
+}
+
+function generateGA() {
+    alert("GA oluşturuldu");
+}
