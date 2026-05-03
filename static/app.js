52744bcf69c30b8dbfc52eae610da9ab0686f624
ozanguldumen-beep <ozanguldumen@gmail.com>
2026-05-03 15:47:37 +0300

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,11 +1,1 @@
-import { createPanel } from "./panel.js";
-import { drawPanel } from "./canvas.js";
-import { initUI } from "./ui.js";
-
-const panel = createPanel(5);
-const canvas = document.getElementById("panelCanvas");
-
-drawPanel(canvas, panel);
-initUI(panel, canvas);
-
-window.KNXDOIT = { panel, canvas };
+Unsupported Media Type
